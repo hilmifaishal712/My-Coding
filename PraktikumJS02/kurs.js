f500de1ecb65ef7fb6503d03838a350e90e66eac
hilmifaishal712 <92616737+hilmifaishal712@users.noreply.github.com>
2021-12-16 23:00:37 +0700

    Add files via upload

---
+++ PraktikumJS02/kurs.js
@@ -0,0 +1,7 @@
+var a;
+function hitung() {
+	a = parseFloat(document.getElementById("opt_$").value);
+	b = a * 14650;
+	document.getElementById('opt_nilai').value = b;
+
+}
---
+++ PraktikumJS02/penjumlahan.js
@@ -0,0 +1,9 @@
+var x;
+var y;
+function hitung_penjumlahan() {
+	x = parseFloat(document.getElementById("bilangan1").value);
+    y = parseFloat(document.getElementById("bilangan2").value);
+	jumlah = x + y;
+	document.getElementById('opt_nilai').value = tambah;
+
+}
